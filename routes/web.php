5d1d8daf02058d5ee4b6444ac153c0291a039aa7
Sean Patrick <2472006@maranatha.ac.id>
2025-03-04 15:21:57 +0700

    Pertemuan 3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DemoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts/index');
 });
+
+Route::get('/1', function () {
+    return view('demo/file1');
+});
+
+Route::get('/2', function () {
+    return view('demo/file2');
+});
+
+Route::get('/3', [DemoController::class, 'index']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
